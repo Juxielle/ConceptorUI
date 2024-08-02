c62a595d31be90083833a00ce50b0c8ed5a6e404
Harold <h.ongagna@initialde.com>
2024-08-02 18:29:16 +0100

    Reformalisation du code

--- ConceptorUI/ViewModels/ComponentModel.cs
+++ ConceptorUI/ViewModels/ComponentModel.cs
@@ -13,6 +13,26 @@ namespace ConceptorUI.ViewModels
             Name = ComponentList.Component;
             Address = string.Empty;
             IsInComponent = true;
+            /*
+                --> Demande de la reference du composant dans la liste des composants, grâce à un évenement
+                --> La reception du composant envoyé se fait dans une autre méthode
+                --> On effectue une affectation directe des propriétés originales
+                --> Le composant appelant (ca) possède lui egalement une liste des proprétés qu'il a modifiées
+                --> Pour les propriétés modifiées, on fait une affectation des props de ca
+                -------------------------
+                --- En réalité, le ca reçoit la ref entère de co en non seulement ses props
+                    --> L'objectif est de construire le ca et ses enfants;
+                    --> Les enfants du ca sont des ca => Tout ca demande à son enfant d'être un ca;
+                    --> Il faut aussi appliquer les contraintes de visibilité, de sélection du co, sur les ca;
+                    --> A chaque fois qu'une propriété modifiable du ca est modifiée, On le signale dans le modèle des propriétés.
+                    
+                --- Reussir à effectuer la copie directe des mises en page:
+                    --> Lors de la reception du co, on effecte les props et les mises en pages.
+                    --> Lorsqu'un co est demandé et qu'il n'est pas encore construit, on le construit d'abord avant d'envoyer
+                    
+                --- Application des contraintes de visibilité et de sélection
+                    --> 
+             */
 
             OnInitialize();
         }
