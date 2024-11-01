bc44074a87a2c883739619dc91122acf7423ef24
Juxielle <juxiharold@gmail.com>
2024-11-01 09:10:31 +0100

    Composant reutilisables - Props configs

---
+++ ConceptorUI/Classes/PropertyConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptorUI.Classes
+{
+    internal class PropertyConfig
+    {
+        public string Name { get; set; }
+        public bool IsSettable { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
