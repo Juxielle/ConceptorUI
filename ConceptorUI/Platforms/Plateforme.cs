e89e3f2a6e5bd3f0a51eeeb725df633190979e5d
Harold <h.ongagna@initialde.com>
2024-12-12 18:05:24 +0100

    Suite platform

--- ConceptorUI/Platforms/Plateforme.cs
+++ ConceptorUI/Platforms/Plateforme.cs
@@ -1,6 +1,8 @@
-﻿namespace ConceptorUI.Platforms;
+﻿using System.Collections.Generic;
+
+namespace ConceptorUI.Platforms;
 
 public class Platform
 {
-    
+    public List<PlatformPage> Pages { get; init; }
 }
