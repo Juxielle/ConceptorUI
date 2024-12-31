7df6441fd26eba1f6c102745ff51bc43e89fb43c
Harold <h.ongagna@initialde.com>
2024-12-31 11:29:03 +0100

    Restore properties

--- ConceptorUI/ViewModels/ReusableComponent/ComponentRestoreProperties.cs
+++ ConceptorUI/ViewModels/ReusableComponent/ComponentRestoreProperties.cs
@@ -1,9 +1,67 @@
-﻿namespace ConceptorUI.ViewModels.ReusableComponent;
+﻿using ConceptorUI.Models;
+using ConceptorUi.ViewModels.Operations;
+
+namespace ConceptorUI.ViewModels.ReusableComponent;
 
 static class ComponentRestoreProperties
 {
     public static void RestoreProperties(this ComponentModel component)
     {
+        var isHorizontal = Alignment.IsHorizontal(component);
         
+        var isVertical = Alignment.IsVertical(component);
+        
+        Alignment.SetSeveralActivations(component);
+        Alignment.SetInvalidValues(component);
+        
+        if (isHorizontal)
+        {
+            Alignment.SetHorizontalOnNull(component);
+        }
+        
+        if (isVertical)
+        {
+            Alignment.SetVerticalOnNull(component);
+        }
+        
+        /* Content */
+        
+
+        /* Status Bar */
+        
+
+        /* Body */
+
+        /* SelfAlignment */
+        SelfAlignment.SetSeveralActivations(component);
+        SelfAlignment.SetInvalidValues(component);
+        
+        var isSelfHorizontal = SelfAlignment.IsHorizontal(component);
+        var isSelfVertical = SelfAlignment.IsVertical(component);
+        
+        var height = component.GetGroupProperties(GroupNames.Transform).GetValue(PropertyNames.Height);
+        var width = component.GetGroupProperties(GroupNames.Transform).GetValue(PropertyNames.Width);
+        
+        if (isSelfHorizontal)
+        {
+            SelfAlignment.SetHorizontalOnNull(component);
+        }
+
+        if (isSelfVertical)
+        {
+            SelfAlignment.SetVerticalOnNull(component);
+        }
+        
+        if (width != "300")
+        {
+            component.SetPropertyValue(GroupNames.Transform, PropertyNames.Width, "300");
+        }
+        
+        if (height != "620")
+        {
+            component.SetPropertyValue(GroupNames.Transform, PropertyNames.Height, "620");
+        }
+        
+        component.Synchronize();
     }
 }
--- ConceptorUI/ViewModels/Window/WindowRestoreProperties.cs
+++ ConceptorUI/ViewModels/Window/WindowRestoreProperties.cs
@@ -1,9 +1,67 @@
-﻿namespace ConceptorUI.ViewModels.Window;
+﻿using ConceptorUI.Models;
+using ConceptorUi.ViewModels.Operations;
+
+namespace ConceptorUI.ViewModels.Window;
 
 static class WindowRestoreProperties
 {
     public static void RestoreProperties(this WindowModel window)
     {
+        var isHorizontal = Alignment.IsHorizontal(window);
+
+        var isVertical = Alignment.IsVertical(window);
+
+        Alignment.SetSeveralActivations(window);
+        Alignment.SetInvalidValues(window);
+
+        if (isHorizontal)
+        {
+            Alignment.SetHorizontalOnNull(window);
+        }
+
+        if (isVertical)
+        {
+            Alignment.SetVerticalOnNull(window);
+        }
         
+        /* Content */
+        
+        
+        /* Status Bar */
+        
+        
+        /* Body */
+
+        /* SelfAlignment */
+        SelfAlignment.SetSeveralActivations(window);
+        SelfAlignment.SetInvalidValues(window);
+
+        var isSelfHorizontal = SelfAlignment.IsHorizontal(window);
+        var isSelfVertical = SelfAlignment.IsVertical(window);
+
+        var height = window.GetGroupProperties(GroupNames.Transform).GetValue(PropertyNames.Height);
+        var width = window.GetGroupProperties(GroupNames.Transform).GetValue(PropertyNames.Width);
+
+        if (isSelfHorizontal)
+        {
+            SelfAlignment.SetHorizontalOnNull(window);
+        }
+
+        if (isSelfVertical)
+        {
+            SelfAlignment.SetVerticalOnNull(window);
+        }
+
+        if (width != "300")
+        {
+            window.SetPropertyValue(GroupNames.Transform, PropertyNames.Width, "300");
+        }
+
+        if (height != "620")
+        {
+            window.SetPropertyValue(GroupNames.Transform, PropertyNames.Height, "620");
+        }
+
+        window.Synchronize();
     }
 }
