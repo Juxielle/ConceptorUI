2b9f3b0203f76af0d5800edbe1ae4ddcbceae4a4
Juxielle <juxiharold@gmail.com>
2025-01-05 22:52:12 +0100

    Platform Rn

--- ConceptorUI/Platforms/ReactNativePlatform/Components/RnView.cs
+++ ConceptorUI/Platforms/ReactNativePlatform/Components/RnView.cs
@@ -5,9 +5,9 @@ namespace ConceptorUI.Platforms.ReactNativePlatform.Components;
 
 public class RnView : RnComponent
 {
-    public string AlignItems { get; set; }
-    public string JustifyContent { get; set; }
-    public string FlexDirection { get; set; }
+    private string AlignItems { get; set; }
+    private string JustifyContent { get; set; }
+    private string FlexDirection { get; set; }
 
     public RnView(CompSerializer compSerializer)
     {
@@ -16,9 +16,57 @@ public class RnView : RnComponent
             ? ColorValue.Transparent.ToString().ToLower()
             : Background;
 
+        var hl = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.Hl);
+        var hc = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.Hc);
+        var hr = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.Hr);
+
+        var vt = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.Vt);
+        var vc = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.Vc);
+        var vb = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.Vb);
+
+        var sb = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.SpaceBetween);
+        var sa = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.SpaceAround);
+        var se = compSerializer.GetGroup(GroupNames.Alignment).GetValue(PropertyNames.SpaceEvery);
+
         if (compSerializer.Name == ComponentList.Row.ToString())
         {
             FlexDirection = "column";
+
+            JustifyContent = vt == "1" ? "flex-start" :
+                vc == "1" ? "center" :
+                vb == "1" ? "flex-end" :
+                sb == "1" ? "space-between" :
+                sa == "1" ? "space-around" :
+                se == "1" ? "space-every" : "flex-start";
+
+            AlignItems = hl == "1" ? "start" :
+                hc == "1" ? "center" :
+                hr == "1" ? "end" : "stretch";
+        }
+        else if (compSerializer.Name == ComponentList.Column.ToString())
+        {
+            FlexDirection = "row";
+
+            JustifyContent = hl == "1" ? "flex-start" :
+                hc == "1" ? "center" :
+                hr == "1" ? "flex-end" :
+                sb == "1" ? "space-between" :
+                sa == "1" ? "space-around" :
+                se == "1" ? "space-every" : "flex-start";
+
+            AlignItems = vt == "1" ? "start" :
+                vc == "1" ? "center" :
+                vb == "1" ? "end" : "stretch";
+        }
+        else if (compSerializer.Name == ComponentList.Container.ToString())
+        {
+            JustifyContent = vt == "1" ? "flex-start" :
+                vc == "1" ? "center" :
+                vb == "1" ? "flex-end" : "flex-start";
+
+            AlignItems = hl == "1" ? "start" :
+                hc == "1" ? "center" :
+                hr == "1" ? "end" : "stretch";
         }
     }
 
