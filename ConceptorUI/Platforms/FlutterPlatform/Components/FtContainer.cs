859a4317bcab22373b66f55b735d9afdee463f16
Juxielle <juxiharold@gmail.com>
2025-01-08 06:00:46 +0100

    Début Platform Flutter

--- ConceptorUI/Platforms/FlutterPlatform/Components/FtContainer.cs
+++ ConceptorUI/Platforms/FlutterPlatform/Components/FtContainer.cs
@@ -1,6 +1,37 @@
-﻿namespace ConceptorUI.Platforms.FlutterPlatform.Components;
+﻿using ConceptorUI.Models;
 
-public class FtContainer
+namespace ConceptorUI.Platforms.FlutterPlatform.Components;
+
+public class FtContainer : FtComponent
 {
+    public string Border { get; set; }
+    public string BorderLeft { get; set; }
+    public string BorderRight { get; set; }
+    public string BorderTop { get; set; }
+    public string BorderBottom { get; set; }
+
+    public string Margin { get; set; }
+
+    public string Padding { get; set; }
+
+    public string BorderRadius { get; set; }
+    public string BorderBottomLeftRadius { get; set; }
+    public string BorderBottomRightRadius { get; set; }
+    public string BorderTopLeftRadius { get; set; }
+    public string BorderTopRightRadius { get; set; }
     
+    protected override string ConvertToString(string space)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Build(CompSerializer compSerializer)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void BuildSingle(FtComponent child)
+    {
+        throw new System.NotImplementedException();
+    }
 }
--- ConceptorUI/Platforms/FlutterPlatform/FtComponent.cs
+++ ConceptorUI/Platforms/FlutterPlatform/FtComponent.cs
@@ -1,11 +1,18 @@
-﻿namespace ConceptorUI.Platforms.FlutterPlatform;
+﻿using System.Collections.Generic;
+using ConceptorUI.Models;
+
+namespace ConceptorUI.Platforms.FlutterPlatform;
 
 public abstract class FtComponent : PlatformComponent
 {
+    protected List<string> Properties = [];
+    
     public override string ToString(string space)
     {
         return ConvertToString(space);
     }
 
     protected abstract string ConvertToString(string space);
+    protected abstract void Build(CompSerializer compSerializer);
+    protected abstract void BuildSingle(FtComponent child);
 }
