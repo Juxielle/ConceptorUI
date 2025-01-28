e1fd73053c4fa95376876e8c76ea23e2734ae749
Harold <h.ongagna@initialde.com>
2025-01-28 09:56:15 +0100

    Refonte

--- UiDesigner/obj/Debug/net6.0-windows/UiDesigner.AssemblyInfo.cs
+++ UiDesigner/obj/Debug/net6.0-windows/UiDesigner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f679808f762f5660a689f7a65b5479dbc5d9ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
