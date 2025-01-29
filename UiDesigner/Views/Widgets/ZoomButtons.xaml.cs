726e692fa0d53192576739da50c3b33f29b22568
Juxielle <juxiharold@gmail.com>
2025-01-29 06:14:06 +0100

    Reforme

--- UiDesigner/Views/Widgets/ZoomButtons.xaml.cs
+++ UiDesigner/Views/Widgets/ZoomButtons.xaml.cs
@@ -1,11 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Input;
 
-namespace UiDesigner.Views.Widgets;
+namespace ConceptorUI.Views.Widgets;
 
-public partial class ZoomButtons : UserControl
+public partial class ZoomButtons
 {
     public ZoomButtons()
     {
         InitializeComponent();
+    }
+    
+    public ICommand ZoomButtonCommand { get; }
+
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(ZoomButtons),
+            new PropertyMetadata(null));
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
     }
 }
--- UiDesigner/obj/Debug/net6.0-windows/UiDesigner.AssemblyInfo.cs
+++ UiDesigner/obj/Debug/net6.0-windows/UiDesigner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21086fc6d16a4f2f93809df4d6c3108fb5fc2140")]
 [assembly: System.Reflection.AssemblyProductAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
