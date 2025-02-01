626714701933de5ba26c59629b473631ab39de40
Harold <h.ongagna@initialde.com>
2025-02-01 08:32:22 +0100

    Ajout modifs

--- UiDesigner/obj/Debug/net6.0-windows/UiDesigner.AssemblyInfo.cs
+++ UiDesigner/obj/Debug/net6.0-windows/UiDesigner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6988aac17ec5ecb56a5a6f56162f9bccb6743e43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e25dded3d77ae39b6a858a6377ed66bf95933d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UiDesigner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
